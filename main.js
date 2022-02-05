98ab4d41a39352c41d2fa6abf6c05ef692899d9b
Soni <soni801_@outlook.com>
2022-02-05 22:13:12 +0100

    feat: slightly improve styling

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+// Execution data
+const api = "http://localhost:5241";
+
+// Function for fetching product list from the API
 async function loadProducts()
 {
-    const products = (await axios.get("http://localhost:5241/products/all")).data;
+    const products = (await axios.get(`${api}/products/all`)).data;
 
     for (const product of products)
     {
         console.log(product);
         document.querySelector("#product-list").innerHTML += `
-            <div class="product">
-                <img src="${product.imageUrl}" alt="${product.name}">
+            <div class="product shadow rounded">
+                <img class="slight-shadow rounded" src="${product.imageUrl}" alt="${product.name}">
                 <div class="horizontal">
                     <h3>${product.name}</h3>
-                    <p><i>${product.price} kr</i></p>
+                    <!-- Format price nicely using https://stackoverflow.com/a/2901298 -->
+                    <p><i>${product.price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} kr</i></p>
                 </div>
                 <p>${product.description}</p>
                 <p class="faded breathe-before"><i>${product.stock} på lager</i></p>
@@ -19,4 +24,5 @@ async function loadProducts()
     }
 }
 
+// Fetch product list on load
 loadProducts().then(() => console.log("Loaded products"));
