5ff566089a2acd31c7e2f098346b18cddebd8fba
Soni <soni801_@outlook.com>
2022-02-06 11:18:02 +0100

    feat: add cart icon to products

--- main.js
+++ main.js
@@ -8,20 +8,26 @@ async function loadProducts()
 
     for (const product of products)
     {
-        console.log(product);
         document.querySelector("#product-list").innerHTML += `
             <div class="product shadow rounded">
-                <img class="slight-shadow rounded" src="${product.imageUrl}" alt="${product.name}">
-                <div class="horizontal">
-                    <h3>${product.name}</h3>
-                    <!-- Format price nicely using https://stackoverflow.com/a/2901298 -->
-                    <p><i>${product.price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} kr</i></p>
+                <div class="info">
+                    <img class="slight-shadow rounded" src="${product.imageUrl}" alt="${product.name}">
+                    <div class="horizontal">
+                        <h3>${product.name}</h3>
+                        <!-- Format price nicely using https://stackoverflow.com/a/2901298 -->
+                        <p><i>${product.price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} kr</i></p>
+                    </div>
+                    <p>${product.description}</p>
                 </div>
-                <p>${product.description}</p>
-                <p class="faded breathe-before"><i>${product.stock} på lager</i></p>
+                <div class="horizontal breathe-before">
+                    <p class="faded"><i>${product.stock} på lager</i></p>
+                    <span class="cart transition"></span>
+                </div>
             </div>
         `;
     }
+
+    $(".cart").load("/assets/icons/cart.svg");
 }
 
 // Fetch product list on load
