10c195e37f66a9f823d1de737bc634948c24f89d
Soni <soni801_@outlook.com>
2022-02-11 11:00:22 +0100

    refactor: use public api url

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Execution data
-const api = "http://localhost:5241";
+const api = "https://api.soni.yessness.com:5000";
 
 // Function for fetching product list from the API
 async function loadProducts()
@@ -14,7 +14,6 @@ async function loadProducts()
                     <img class="slight-shadow rounded" src="${product.imageUrl}" alt="${product.name}">
                     <div class="horizontal">
                         <h3>${product.name}</h3>
-                        <!-- Format price nicely using https://stackoverflow.com/a/2901298 -->
                         <p><i>${product.price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} kr</i></p>
                     </div>
                     <p>${product.description}</p>
@@ -32,3 +31,7 @@ async function loadProducts()
 
 // Fetch product list on load
 loadProducts().then(() => console.log("Loaded products"));
+
+/* Resources
+    - Number formatting: https://stackoverflow.com/a/2901298
+ */
