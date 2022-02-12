e2fa657b62f45d4b9c2155b7fa711ee75bf5a499
Soni <soni801_@outlook.com>
2022-02-12 10:39:01 +0100

    fix: improve error display

--- main.js
+++ main.js
@@ -44,41 +44,49 @@ $("#passphrase").keyup(e =>
 $("#login-button").click(async function ()
 {
     // Remove error display
-    document.querySelector("#incorrect").style.display = "none";
+    document.querySelector("#incorrect").innerHTML = "";
 
     // Store credentials
     const username = document.querySelector("#username").value;
     const passphrase = document.querySelector("#passphrase").value;
 
-    // Make a request to the API
-    const result = (await axios({
-        method: "get",
-        url: `${api}/auth`,
-        headers: {
-            user: username,
-            pass: passphrase
-        }
-    })).data;
-
-    // Store success
-    const success = result !== "";
-    console.log(success ? "Login successful" : "Incorrect username/password");
-
-    if (success)
+    try
     {
-        // Store user details
-        localStorage["user"] = JSON.stringify((await axios({
+        // Make a request to the API
+        const result = (await axios({
             method: "get",
-            url: `${api}/users`,
+            url: `${api}/auth`,
             headers: {
-                token: result
+                user: username,
+                pass: passphrase
             }
-        })).data);
+        })).data;
 
-        // Redirect to front page
-        window.location.replace("/");
+        // Store success
+        const success = result !== "";
+        console.log(success ? "Login successful" : "Incorrect username/password");
+
+        if (success)
+        {
+            // Store user details
+            localStorage["user"] = JSON.stringify((await axios({
+                method: "get",
+                url: `${api}/users`,
+                headers: {
+                    token: result
+                }
+            })).data);
+
+            // Redirect to front page
+            window.location.replace("/");
+        }
+        // Unsuccessful login
+        else document.querySelector("#incorrect").innerHTML = "Feil brukernavn eller passord";
     }
-    else document.querySelector("#incorrect").style.display = "unset";
+    catch // The API responds with an error
+    {
+        document.querySelector("#incorrect").innerHTML = "Vennligst fyll inn alle feltene";
+    }
 });
 
 // Fetch product list on load
