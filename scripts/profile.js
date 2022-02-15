9e77c3846666012eb063d35710f0b9324e9e2f06
Soni <soni801_@outlook.com>
2022-02-15 21:56:57 +0100

    feat: add user deletion

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,7 @@
+// Variables
+let deletingTimer = 0;
+let deletingCountdownInterval;
+
 // Load user info
 if (localStorage["user"])
 {
@@ -8,3 +12,42 @@ if (localStorage["user"])
     document.querySelector("#account-info :nth-child(2)").innerHTML = `<i>${user.email}</i>`;
     document.querySelector("#account-info :nth-child(6)").innerHTML = `<i>(+47) ${user.phoneNumber.toString().substring(0, 3)} ${user.phoneNumber.toString().substring(3, 5)} ${user.phoneNumber.toString().substring(5)}</i>`;
 }
+
+// Add event listener to delete button
+$("#delete-button").click(async () =>
+{
+    if (deletingTimer === 0)
+    {
+        deletingTimer = 5;
+        document.querySelector("#delete-button").innerHTML = `Trykk igjen for å bekrefte... ${deletingTimer}`;
+        deletingCountdownInterval = setInterval(() =>
+        {
+            deletingTimer--;
+            if (deletingTimer > 0) document.querySelector("#delete-button").innerHTML = `Trykk igjen for å bekrefte... ${deletingTimer}`;
+            else
+            {
+                clearInterval(deletingCountdownInterval);
+                document.querySelector("#delete-button").innerHTML = "Slett konto";
+            }
+        }, 1000);
+    }
+    else
+    {
+        document.querySelector("#incorrect").innerHTML = "";
+
+        const success = (await axios({
+            method: "delete",
+            url: `${api}/users`,
+            headers: {
+                token: user.credentials.token
+            }
+        })).data;
+
+        if (success)
+        {
+            localStorage.removeItem("user");
+            window.location.replace("/");
+        }
+        else document.querySelector("#incorrect").innerHTML = "Noe gikk galt.";
+    }
+});
